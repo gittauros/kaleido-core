fef289ee94dafdbc4ea75866e8dd895ebadc6f76
gittauros <zhy12081992@126.com>
2016-04-10 12:19:57 +0800

    保存文件路径设置

--- src/test/java/com/tauros/kaleido/core/test/Demo.java
+++ src/test/java/com/tauros/kaleido/core/test/Demo.java
@@ -1,8 +1,5 @@
 package com.tauros.kaleido.core.test;
 
-import com.tauros.kaleido.core.download.DownloaderInfo;
-import com.tauros.kaleido.core.task.TaskInfo;
-
 import java.io.Serializable;
 
 /**
@@ -12,19 +9,7 @@ public class Demo implements Serializable {
 	private static final long serialVersionUID = -2777120453873339539L;
 
 
-	static class FinalInfo extends DownloaderInfo {
-		@Override
-		protected String selfInfo() {
-			return "c";
-		}
-	}
-
 	public static void main(String[] args) {
-		TaskInfo<TaskInfo> taskInfo = new TaskInfo<>();
-		DownloaderInfo<DownloaderInfo> downloaderInfo = new DownloaderInfo<>();
-		FinalInfo finalInfo = new FinalInfo();
-		taskInfo.setExtraInfo(downloaderInfo);
-		downloaderInfo.setExtraInfo(finalInfo);
-		System.out.println(taskInfo.fullInfo());
+		System.out.println(System.getProperty("user.home"));
 	}
 }
