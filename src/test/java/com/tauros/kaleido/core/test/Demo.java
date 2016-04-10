71236f60abb319f759a253dc13093797e5b8a9d5
gittauros <zhy12081992@126.com>
2016-04-10 13:45:49 +0800

    zhy

--- src/test/java/com/tauros/kaleido/core/test/Demo.java
+++ src/test/java/com/tauros/kaleido/core/test/Demo.java
@@ -1,6 +1,8 @@
 package com.tauros.kaleido.core.test;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.util.Scanner;
 
 /**
  * Created by tauros on 2016/4/9.
@@ -8,8 +10,22 @@ import java.io.Serializable;
 public class Demo implements Serializable {
 	private static final long serialVersionUID = -2777120453873339539L;
 
+	private static String trans(String str) {
+		int index = str.indexOf("?");
+		String pre = str.substring(0, index);
+		String suf = str.substring(index, str.length());
+		pre = pre.substring(pre.indexOf("$") + 1, pre.length());
+		pre = pre.replaceAll("=2", ":");
+		pre = pre.replaceAll("=1", ".");
 
-	public static void main(String[] args) {
-		System.out.println(System.getProperty("user.home"));
+		return pre + suf;
+	}
+
+	public static void main(String[] args) throws IOException {
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNext()) {
+			String str = scanner.nextLine();
+			System.out.println(trans(str));
+		}
 	}
 }
